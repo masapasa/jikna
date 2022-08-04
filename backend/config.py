7e3f8cbb13e0ca2015434ddaa45ac6c967168e6b
masapasa <apyakurel@gmail.com>
2022-08-04 23:58:21 +0200

    init

--- backend/config.py
+++ backend/config.py
@@ -3,4 +3,5 @@ NUM_DOCS = 30000
 DATA_FILE = "./data/community.csv"
 DATABASE_NAME = "covid-qa.db"
 TABLE_NAME = "questions"
-HOST = "https://covid-kb-5b6395f830.wolf.jina.ai"
+#HOST = "https://covid-kb-5b6395f830.wolf.jina.ai"
+HOST = "https://909ce646f0.wolf.jina.ai"
